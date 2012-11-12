c01a95d79c8f4104d435e0dee931323a1d24dc3a
Eric-Xu <e.xulander@gmail.com>
2012-11-12 11:03:51 -0500

    Started new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditOnRails::Application.config.secret_token = 'eebe43c127a8c5b8ab770c1ea936cdeac773f34daa20cb1a9f5ec09dc64187daa558149b9f179b8df2989ee5e2f7d51d2437a6b6669238376cfd3601eaaaaba4'
