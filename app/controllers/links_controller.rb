a4a2a4359ebadd38ce52b0b95d1ebaafdc6e41cd
Eric-Xu <e.xulander@gmail.com>
2012-11-13 22:46:37 -0500

    Display links after submitting and on homepage

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   def show
+  	@link = Link.where(:id => params[:link]).first
   end
 
   def new
@@ -7,5 +8,16 @@ class LinksController < ApplicationController
   end
 
   def create
+  	@link = Link.new(params[:link])
+
+  	respond_to do |format|
+  	  if @link.save
+   	    format.html { render :action => "show" }
+  	    format.json { render :json => @link }
+  	  else
+  	  	format.html { render :action => "new" }
+  	  	format.json { render :json => @link.error, :status => :unprocessable_entity }
+  	  end
+  	end
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :user_id, :url, :title
+  attr_accessible :url, :title
   belongs_to :user
 end
