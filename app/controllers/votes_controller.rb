f578338ef247fc58e5303711fec9a2d86a9a2e5c
Eric-Xu <e.xulander@gmail.com>
2012-11-19 13:47:14 -0500

    Added vote validation for users.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,17 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-  	@vote = current_user.votes.create(params[:vote])
-  	redirect_to :back
+	@vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+	if @vote
+	  @vote.up = params[:vote][:up]
+	  @vote.save
+	else
+	  @vote = current_user.votes.create(params[:vote])
+	end
+	redirect_to :back
+	# Can also be written using fancier boolean logic:
+	# @vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+	# @vote ||= current_user.votes.create(params[:vote])
+	# @vote.update_attributes(:up => params[:vote][:up])
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,9 @@ class Vote < ActiveRecord::Base
   attr_accessible :up, :user_id, :link_id
   belongs_to :user
   belongs_to :link 
+
+  # Specify that new votes can only be saved if the user_id 
+  # and link_id combo haven't been used before.
+  validates :user_id, :uniqueness => { :scope => :link_id }
+
 end
