518ddbe4a74ce8b0017806c41427c3584492803e
Eric-Xu <e.xulander@gmail.com>
2012-11-19 11:59:14 -0500

    Created votes model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :username, :password, :password_confirmation, :remember_me
   has_many :links
   has_many :comments
+  has_many :votes
 end
---
+++ app/models/vote.rb
@@ -0,0 +1,6 @@
+class Vote < ActiveRecord::Base
+  # attr_accessible :title, :body
+  attr_accessible :up, :user_id, :link_id
+  belongs_to :user
+  belongs_to :link 
+end
